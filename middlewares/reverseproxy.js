92757c17c22aec776fdb228ad2cc537c4d86fabe
MatthD <matthias.dieudonne@gmail.com>
2015-01-22 15:13:17 +0100

    Added ACE json editor for code + changed way to redirect proxy domain

--- middlewares/reverseproxy.js
+++ middlewares/reverseproxy.js
@@ -21,41 +21,66 @@ module.exports = function() {
         var proxy       = httpProxy.createProxyServer({}),
             redirected  = false;
 
-        // IF Reverse proxy used
-        if ( (domainProxy) && (req.headers['x-forwarded-server']) && (req.headers['x-forwarded-server'] === domainProxy ) ) {
+        // IF NO ENV VARIABLE PROXY
+        if(!domainProxy){
 
-            /// IF there is the name host (subdomain)
-            if ((req.headers['x-forwarded-host'])) {
+            console.log('Pas de variable');
 
-                var subdomain = (req.headers['x-forwarded-host']).split('.'+ domainProxy); // split subdomain
-                //console.log(subdomain[0]);
+            //IF HEADER DOMAIN SEND
+            if ( (req.headers['x-forwarded-server']) ){
+                res.render('404' , { title: 'Not Found !', path: '/', userName: req.user });
+            }
+            // ACCES ADMIN
+            else{
+                next();
+            }
 
-                // For each instances config
-                Object.keys(config.instances, function (key, value) {
+        }
+        else{
+            //IF HEADER DOMAIN SEND
+            if ( (req.headers['x-forwarded-server']) ) {
 
-                    //console.log('valueID : ', value.id + '  / DOMAIN : ', subdomain[0]);
+                //IF HEADER DOMAIN = ENV PROXY
+                if (req.headers['x-forwarded-server'] === domainProxy ){
 
-                    // If technicalname = subdomain
-                    if ((value.id) && (value.id == subdomain[0])) {
+                    /// IF there is the name host (subdomain)
+                    if ((req.headers['x-forwarded-host'])) {
 
-                        var url = 'http://127.0.0.1:' + value.port;
-                        proxy.web(req, res, { target: url });
+                        var subdomain = (req.headers['x-forwarded-host']).split('.'+ domainProxy); // split subdomain
 
-                        redirected = true;
+                        // For each instances config
+                        Object.keys(config.instances, function (key, value) {
+
+                            // If technicalname = subdomain
+                            if ((value.id) && (value.id == subdomain[0])) {
+
+                                var url = 'http://127.0.0.1:' + value.port;
+                                proxy.web(req, res, { target: url });
+
+                                redirected = true;
+
+                            }
+
+                        });
+
+                        //IF NO INSTANCES HAVE REDIRECTED
+                        if(!redirected){
+                            res.render('404' , { title: 'Not Found !', path: '/', userName: req.user });
+                        }
 
                     }
-
-                });
-
-                if(!redirected){
+                }
+                //IF HEADER DOMAIN != ENV PROXY
+                else{
                     res.render('404' , { title: 'Not Found !', path: '/', userName: req.user });
                 }
 
             }
+            //IF HEADER DOMAIN NOT SEND
+            else{
+                next();
+            }
 
-        }
-        else {
-            next();
         }
 
 
